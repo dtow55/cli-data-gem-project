7fe005253d01de747666500a10617af29567dbdf
David Towster <djt4xe@virginia.edu>
2018-01-22 16:14:02 -0800

    Note: code changes desribed in previous commmit can be found in this commit (I forgot to 'git add .'. Additionally, attribute variables of resort.rb were determined

--- lib/zrankings/resort.rb
+++ lib/zrankings/resort.rb
@@ -2,6 +2,9 @@ require 'pry'
 
 def Zrankings::Resort
 
+    attr_accessor :name, :location, :url, :snowfall, :peak_elevation,
+                :vertical, :acres, :rank, :multipass, :description
 
+    @@all = []
 
 end
--- lib/zrankings/scraper.rb
+++ lib/zrankings/scraper.rb
@@ -3,15 +3,15 @@ require 'pry'
 def Zrankings::Scraper
 
     def get_page
-
+        #opens zrankings.com using open-uri and assigns it to a 'doc' variable using Nokogiri
     end
 
     def scrape_resorts
-
+        #calls #get_page and returns an array of XML objects from which we will scrape resorts
     end
 
     def create_resorts
-
+        #calls #scrape_resorts and a Resort class method to create Resort objects with information
+        #scraped from Nokogiri XML
     end
-
 end
