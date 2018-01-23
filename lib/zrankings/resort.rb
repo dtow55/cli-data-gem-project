2116b50c8a12af5fdb8cffb57d4385b4bcf2014e
David Towster <djt4xe@virginia.edu>
2018-01-22 17:25:08 -0800

    wrote initialize method for Resort object

--- lib/zrankings/resort.rb
+++ lib/zrankings/resort.rb
@@ -1,4 +1,5 @@
 require_relative '../zrankings.rb'
+require_relative './version.rb'
 require 'pry'
 
 class Zrankings::Resort
@@ -8,10 +9,19 @@ class Zrankings::Resort
 
     @@all = []
 
+    def initialize(name=nil, location=nil, rank=nil, url=nil, description=nil)
+        @name = name
+        @location = location
+        @rank = rank
+        @url = url
+        @description = description
+        @@all << self
+    end
+
     def self.all
         @@all
     end
 end
 
-#r = Resort.new
-#binding.pry
+r = Zrankings::Resort.new
+binding.pry
