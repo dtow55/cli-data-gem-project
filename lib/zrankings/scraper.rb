cc28bebcb1eea2f29f97d5c1f51ba65eae838d5b
David Towster <djt4xe@virginia.edu>
2018-01-22 17:38:27 -0800

    more work on  #create_resorts in scraper.rb

--- lib/zrankings/scraper.rb
+++ lib/zrankings/scraper.rb
@@ -16,9 +16,14 @@ class Zrankings::Scraper
     def create_resorts
         #calls #scrape_resorts and a Resort class method to create Resort objects with information
         #scraped from Nokogiri XML
-        scrape_resorts.each do |resort|
-            name = resort.css("td#name-rank-index a").text
-            location = resort.css("td#name-rank-index span").text
+        scrape_resorts.each do |resort_xml|
+            name = resort_xml.css("td#name-rank-index a").text
+            location = resort_xml.css("td#name-rank-index span").text
+            rank = resort_xml.css("tr.top-rowdy td")[0].text
+            url = "https://www.zrankings.com/" + resort_xml.css("td#name-rank-index a").attribute('href').value
+            description = resort_xml.css("div.index-table-text-box")[0].text.strip
+
+            Zrankings::Resort.new(name, location, rank, url, description)
 
             binding.pry
         end
